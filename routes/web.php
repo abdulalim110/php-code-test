606a4ca30361b5f0463827edaddf950f88ba2174
Abdul Alim <abdul.aalim110@gmail.com>
2026-01-24 16:09:07 +0700

    feat: Update root route to return JSON response with app details and server time

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'app' => 'Binar Technical Test API',
+        'version' => '1.0.0',
+        'status' => 'running',
+        'server_time' => now()->toIso8601String(),
+        'maintainer' => 'Abdul Alim'
+    ]);
 });
